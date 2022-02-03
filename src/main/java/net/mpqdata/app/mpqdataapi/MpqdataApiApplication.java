ec804ed2367ef4fb18cfe6a69f0d892590d05eeb
Chris Spears <zometer@gmail.com>
2022-02-02 23:48:21 -0600

    Initial commit.

---
+++ src/main/java/net/mpqdata/app/mpqdataapi/MpqdataApiApplication.java
@@ -0,0 +1,13 @@
+package net.mpqdata.app.mpqdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpqdataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpqdataApiApplication.class, args);
+	}
+
+}
