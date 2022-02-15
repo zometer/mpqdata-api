b9b2e183055d88fc6f65c123c758dff621bfd482
Chris Spears <zometer@gmail.com>
2022-02-15 16:32:38 -0600

    #15: Adding JPA annotations to AllianceMember class

--- src/main/java/net/mpqdata/app/mpqdataapi/model/domain/AllianceMember.java
+++ src/main/java/net/mpqdata/app/mpqdataapi/model/domain/AllianceMember.java
@@ -1,19 +1,30 @@
 package net.mpqdata.app.mpqdataapi.model.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 import lombok.Data;
 
+@Entity
 @Data
+@Table(schema="mpq_data", name="player")
 public class AllianceMember {
 
-	@JsonAlias("name")
+	@Id
+	private Long playerId;
+
+	@JsonAlias({"name", "player_name"})
 	private String playerName;
 
-	@JsonAlias("guid")
+	@JsonAlias({"guid", "player_guid"})
 	private String playerGuid;
 
-	@JsonAlias("role")
+	@JsonAlias({"role", "alliance_role"})
+	@Transient
 	private String allianceRole;
 
 }
