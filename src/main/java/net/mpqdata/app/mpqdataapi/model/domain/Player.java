f6a61a3db6de80ea929fb5bb4439d5ed2747b093
Chris Spears <zometer@gmail.com>
2022-02-15 18:03:15 -0600

    #15: Adding lombok constructors and @GeneratedValue JPA annotation

--- src/main/java/net/mpqdata/app/mpqdataapi/model/domain/Player.java
+++ src/main/java/net/mpqdata/app/mpqdataapi/model/domain/Player.java
@@ -1,20 +1,27 @@
 package net.mpqdata.app.mpqdataapi.model.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Table(schema="mpq_data", name="player")
 public class Player {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long playerId;
 
 	@JsonAlias({"name", "player_name"})
