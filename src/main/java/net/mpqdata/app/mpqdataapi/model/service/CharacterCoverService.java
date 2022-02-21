681a837363d7164c70744fd5226f9c41b112ca13
Chris Spears <zometer@gmail.com>
2022-02-21 10:26:11 -0600

    #18: Adding CharacterCover domain, repo and service.

---
+++ src/main/java/net/mpqdata/app/mpqdataapi/model/service/CharacterCoverService.java
@@ -0,0 +1,27 @@
+package net.mpqdata.app.mpqdataapi.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.Setter;
+import net.mpqdata.app.mpqdataapi.model.domain.CharacterCover;
+import net.mpqdata.app.mpqdataapi.model.repository.CharacterCoverRepository;
+
+@Service
+public class CharacterCoverService {
+
+	@Autowired
+	@Setter
+	private CharacterCoverRepository characterCoverRepository;
+
+	public List<CharacterCover> fetchUnprocessedCovers() {
+		return characterCoverRepository.findByCompleteFalse();
+	}
+
+	public CharacterCover save(CharacterCover cover) {
+		return characterCoverRepository.save(cover);
+	}
+
+}
