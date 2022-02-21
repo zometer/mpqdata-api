ba202ea035b2c0d30c1aff657fdf20018333eb1f
Chris Spears <zometer@gmail.com>
2022-02-21 13:02:50 -0600

    #18: Removing unused repo method.

--- src/main/java/net/mpqdata/app/mpqdataapi/model/repository/CharacterCoverRepository.java
+++ src/main/java/net/mpqdata/app/mpqdataapi/model/repository/CharacterCoverRepository.java
@@ -2,23 +2,14 @@ package net.mpqdata.app.mpqdataapi.model.repository;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import net.mpqdata.app.mpqdataapi.model.domain.CharacterCover;
 
 @Repository
-public interface CharacterCoverRepository extends JpaRepository<CharacterCover, String>{
+public interface CharacterCoverRepository extends JpaRepository<CharacterCover, Long>{
 
 	public List<CharacterCover> findByCompleteFalse();
-
-	@Modifying
-	@Transactional
-	@Query(nativeQuery = true)
-	public void insertForNewCharacters();
 
 }
