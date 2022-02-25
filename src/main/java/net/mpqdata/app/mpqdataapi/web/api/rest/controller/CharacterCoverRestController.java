a22c5a3a952f243e6d0490eb392a754b5922d6cc
Chris Spears <zometer@gmail.com>
2022-02-25 09:46:28 -0600

    #18: adding seachCoverImages() endpoint

--- src/main/java/net/mpqdata/app/mpqdataapi/web/api/rest/controller/CharacterCoverRestController.java
+++ src/main/java/net/mpqdata/app/mpqdataapi/web/api/rest/controller/CharacterCoverRestController.java
@@ -1,15 +1,20 @@
 package net.mpqdata.app.mpqdataapi.web.api.rest.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Setter;
+import net.mpqdata.app.mpqdataapi.model.domain.ImageApiSearchRequest;
 import net.mpqdata.app.mpqdataapi.model.domain.CharacterCover;
+import net.mpqdata.app.mpqdataapi.model.domain.ImageApiSearchResult;
+import net.mpqdata.app.mpqdataapi.model.domain.ImageApiSearchResult.RemoteApi;
 import net.mpqdata.app.mpqdataapi.model.service.CharacterCoverService;
 
 @RestController
@@ -20,7 +25,7 @@ public class CharacterCoverRestController {
 	private CharacterCoverService characterCoverService;
 
 	@RequestMapping(method=RequestMethod.POST, value="/api/rest/v{version}/covers")
-	public CharacterCover save(CharacterCover cover) {
+	public CharacterCover save(@RequestBody CharacterCover cover) {
 		return characterCoverService.save(cover);
 	}
 
@@ -32,6 +37,21 @@ public class CharacterCoverRestController {
 	@RequestMapping("/api/rest/v{version}/covers/{characterCoverId}")
 	public CharacterCover fetchCover(@PathVariable("characterCoverId") long characterCoverId) {
 		return characterCoverService.fetchCover(characterCoverId);
+	}
+
+	@RequestMapping(method=RequestMethod.POST, value="/api/rest/v{version}/covers/images/search")
+	public List<ImageApiSearchResult> seachCoverImages(@RequestBody ImageApiSearchRequest request) {
+
+		if (request.getRemoteApi() != null && request.getIssueId() != null) {
+			return characterCoverService.searchForCoverImagesByIssueId(request.getRemoteApi(), request.getIssueId());
+		}
+
+		List<ImageApiSearchResult> marvelResults = characterCoverService.searchForCoverImages(RemoteApi.MARVEL, request);
+		List<ImageApiSearchResult> gcdResults = characterCoverService.searchForCoverImages(RemoteApi.GCD, request);
+
+		List<ImageApiSearchResult> results = new ArrayList<>(marvelResults);
+		results.addAll(gcdResults);
+		return results;
 	}
 
 }
