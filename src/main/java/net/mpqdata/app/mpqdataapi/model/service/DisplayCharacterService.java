0f95a9e9cf79d601a066ec58d7826953906dde4d
Chris Spears <zometer@gmail.com>
2022-03-02 14:48:12 -0600

    #5: removing duplicate abilities from DisplayCharacter

--- src/main/java/net/mpqdata/app/mpqdataapi/model/service/DisplayCharacterService.java
+++ src/main/java/net/mpqdata/app/mpqdataapi/model/service/DisplayCharacterService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.Setter;
 import net.mpqdata.app.mpqdataapi.MpqDataApiException;
+import net.mpqdata.app.mpqdataapi.model.domain.DisplayAbility;
 import net.mpqdata.app.mpqdataapi.model.domain.DisplayCharacter;
 import net.mpqdata.app.mpqdataapi.model.domain.RosteredCharacter;
 import net.mpqdata.app.mpqdataapi.model.repository.DisplayCharacterRepository;
@@ -42,25 +43,52 @@ public class DisplayCharacterService {
 		List<DisplayCharacter> outChars = new ArrayList<>();
 		characters.stream().forEach( c -> {
 			DisplayCharacter dbChar = dbChars.stream()
-				.filter( dbc -> dbc.getEffectiveLevel() == c.getEffectiveLevel() && dbc.getMpqCharacterId().equals( c.getMpqCharacterId() ))
+				.filter( dbc -> dbc.getEffectiveLevel() == c.getEffectiveLevel() && dbc.getMpqCharacterKey().equals( c.getMpqCharacterKey() ))
 				.map(dbc -> cloneFactory.clone(dbc, DisplayCharacter.class) )
 				.map(dbc -> {
-					logger.debug(c.getMpqCharacterId() + "-" + c.getEffectiveLevel() + " / " + dbc.getMpqCharacterId() + "-" + dbc.getEffectiveLevel());
+					logger.debug(c.getMpqCharacterKey() + "-" + c.getEffectiveLevel() + " / " + dbc.getMpqCharacterKey() + "-" + dbc.getEffectiveLevel());
 					return dbc;
 				})
 				.findFirst()
 				.orElseThrow( () -> {
-					return new MpqDataApiException("Error finding db match for: " + c.getMpqCharacterId() + "-" + c.getEffectiveLevel());
+					return new MpqDataApiException("Error finding db match for: " + c.getMpqCharacterKey() + "-" + c.getEffectiveLevel());
 				})
 			;
 			dbChar.setInstanceId( c.getInstanceId() );
-			for (int i=0; i < c.getAbilityLevels().size() ; i++) {
-				dbChar.getAbilities().get(i).setAbilityLevel( convertRawAbilityLevel( c.getAbilityLevels().get(i) ) );
+			removeDuplicates(dbChar.getAbilities());
+			for (int i=0; i < c.getAbilityLevels().size(); i++) {
+				int abilityLevel = convertRawAbilityLevel(c.getAbilityLevels().get(i));
+				dbChar.getAbilities().get(i).setAbilityLevel( abilityLevel );
 			}
 			outChars.add(dbChar);
 		});
 
 		return outChars;
+	}
+
+	private void removeDuplicates(List<DisplayAbility> abilities) {
+		List<Integer> abilityIds = new ArrayList<>();
+		List<DisplayAbility> distinct = new ArrayList<>();
+		for (DisplayAbility ability : abilities) {
+			if (abilityIds.contains( ability.getAbilityId() )) {
+				continue;
+			}
+			abilityIds.add( ability.getAbilityId() );
+			distinct.add(ability);
+		}
+		distinct.sort( (a, b) -> {
+			int value = zeroForNull(a.getAbilitySet()).compareTo( zeroForNull(b.getAbilitySet()) );
+			if (value != 0) {
+				return value;
+			}
+			return Integer.valueOf(a.getOrdinalPosition()).compareTo( b.getOrdinalPosition() );
+		});
+		abilities.clear();
+		abilities.addAll(distinct);
+	}
+
+	private Integer zeroForNull(Integer val) {
+		return val == null ? 0 : val;
 	}
 
 	public int convertRawAbilityLevel(int rawAbilityLevel) {
